df9b1b61627d36c3de14e4a067e4da07d777d660
Brad Roberts <braddr@puremagic.com>
2013-03-16 02:05:45 -0700

    add bash config files

---
+++ src/bash-prompt.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+# set a fancy prompt (non-color, unless we know we "want" color)
+case "$TERM" in
+    screen|screen.linux|xterm-color) color_prompt=yes;;
+esac
+
+# uncomment for a colored prompt, if the terminal has the capability; turned
+# off by default to not distract the user: the focus in a terminal window
+# should be on the output of commands, not on the prompt
+#force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+	# We have color support; assume it's compliant with Ecma-48
+	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+	# a case would tend to support setf rather than setaf.)
+	color_prompt=yes
+    fi
+fi
+
+if [ -x $HOME/.git-prompt.sh ]; then
+    . $HOME/.git-prompt.sh
+    gitprompt=`__git_ps1 " (%s)"`
+fi
+
+curdir="${PWD/${HOME}/~}"
+
+case "${curdir}" in
+    \~/sandbox/dotfiles*)
+        curdir="dotfiles${curdir/~\/sandbox\/dotfiles/}"
+        ;;
+    \~/sandbox/d/source/*)
+        curdir="d${curdir/~\/sandbox\/d\/source\// }"
+        ;;
+    *)
+        ;;
+esac
+
+if [ "$color_prompt" = yes ]; then
+    color_one="\[\e[01;32m\]"
+    color_two="\[\e[01;36m\]"
+    color_reset="\[\e[00m\]"
+fi
+
+echo -n "${debian_chroot:+($debian_chroot)}${color_one}\u@\h${color_reset}:${color_two}${curdir}${color_reset}${gitprompt}\$ "
+
